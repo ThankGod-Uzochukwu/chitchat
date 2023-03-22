26095d77167a719425dd1c277e769071c19c2ed4
TeeGee-TechBro <allin1acct.com@gmail.co>
2023-03-22 18:27:57 +0100

    first commit

---
+++ trying.php
@@ -0,0 +1,33 @@
+<?php
+$name = "TeeGee ";
+$add='is ';
+$age= 9+1;
+$space=" ";
+echo $name.$add. $age;
+
+/*define("me", "Zoe");
+echo "<br>". me;
+$name= ", David..."; 
+function getName(){
+    global $name;
+    echo $name;
+}
+getName();*/
+if ($age<=18){
+    echo "<br>" ."GET THE FVCK OUT!!!!!";
+}
+else{
+    echo "<br>" . "YOU'RE WELCOMED!!!!!";
+}
+/*echo "<br>" ."<br>" . $_SERVER['SCRIPT_NAME'];
+echo "<br>" ."<br>" . $_SERVER['HTTP_HOST'] . "<br>";*/
+echo "<br>";
+echo "Logged in Successfully!";
+
+ 
+
+
+
+
+
+?>
